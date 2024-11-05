9973bcec3f226d03752e2c916757186a454c9b85
zac7518 <149940553+zac7518@users.noreply.github.com>
2024-11-05 10:14:12 +0100

    c

--- script.js
+++ script.js
@@ -1,45 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const slides = document.querySelectorAll('.slide');
-    const prevBtn = document.getElementById('prevBtn');
-    const nextBtn = document.getElementById('nextBtn');
-
-    let currentIndex = 0;
-
-    // Function to display current slide
-    function showSlide(index) {
-        slides.forEach((slide, i) => {
-            if (i === index) {
-                slide.style.transform = `translateX(0)`;
-            } else {
-                slide.style.transform = `translateX(${100 * (i - index)}%)`;
-            }
-        });
-    }
-
-    // Event listener for previous button
-    prevBtn.addEventListener('click', () => {
-        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-        showSlide(currentIndex);
-    });
-
-    // Event listener for next button
-    nextBtn.addEventListener('click', () => {
-        currentIndex = (currentIndex + 1) % slides.length;
-        showSlide(currentIndex);
-    });
-
-    // Initially display the first slide
-    showSlide(currentIndex);
-});
-
-// Charger le header
+// Header
 fetch('header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header').innerHTML = data;
     });
 
-// Charger le footer
+// Footer
 fetch('footer.html')
     .then(response => response.text())
     .then(data => {
@@ -47,17 +13,14 @@ fetch('footer.html')
     });
 
 
-// SLIDES
-
+// Slides compétences
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
@@ -78,6 +41,7 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
 }
 
+// JSON
 document.addEventListener('DOMContentLoaded', () => {
     fetch('content.json')
         .then(response => response.json())
